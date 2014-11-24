c16185c962a0a7985d9cef3c1f158b00b2570b73
zhaoliang <zhaoliangwa@qq.com>
2014-11-24 20:04:44 +0800

    updated,programming soled,there is a bug , if the password has some repeated letter,then failed

--- p079.py
+++ p079.py
@@ -15,7 +15,7 @@ letterLs=set()
 for line in inputFile:
     if int(line)>0:
         for le in str(int(line)):
-            letterLs.add(le)
+            letterLs.add(int(le))
         pwdLs.append(int(line))
 
         
@@ -24,15 +24,35 @@ print "this shows the minimal width of password : "+str(len(letterLs))
 
 lens=len(letterLs)
 flag=False
-base=[]  #存数字的字符格式
-#insert(i,x) 把元素x 插入到位置i
-#index(x) 返回数组中第一个值为x的位置。如果没有匹配的元素会抛出一个错误
-#count(x) 返回x在数组中出现的次数
+base=[0,1,2,3,4,5,6,7,8,9]  
+nLen=len(pwdLs)
+n=0
+while n<nLen:
+    sD=pwdLs[n]
+    s1=int(str(sD)[0:1])
+    s2=int(str(sD)[1:2])
+    i1=base.index(s1)
+    i2=base.index(s2)
+    if i1>i2:
+        tmp=base[i1]
+        base[i1]=base[i2]
+        base[i2]=tmp
+    i2=base.index(s2)
+    s3=int(str(sD)[2:3])
+    i3=base.index(s3)
+    if i2>i3:
+        tmp=base[i2]
+        base[i2]=base[i3]
+        base[i3]=tmp
+    n+=1
 
-newPwd=list(pwdLs)
-newPwd.sort()            
-        
-print newPwd
+pwd=[]    
+for x in base:
+    if x in letterLs:
+        pwd.append(x)
+print pwd
+
+    
 
 endtime = datetime.datetime.now()
 print('All spent '+str((endtime-starttime).seconds)+' seconds')
