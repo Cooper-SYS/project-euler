8aafa7594223ad0883d9ab336e0eb6c9fd42a0f5
zhaoliang <zhaoliangwa@qq.com>
2014-12-03 17:58:59 +0800

    updated p058

--- p058.py
+++ p058.py
@@ -1,42 +1,67 @@
 #coding=utf-8
 #!/usr/bin/python
 #python2.7.8
-
+from __future__ import division
 import datetime
 
-def getNum(n,r):
-    top=1
-    botLeft=1 
-    botRight=1
-    
-    for x in range(2,n+1):
-        top *= x 
-    for x in range(2,r+1):
-        botLeft *= x
-    for x in range(2,n-r+1):
-        botRight *= x
-    return int(top/botLeft/botRight)      
-                
+import math
+'''
+推导过程
+根据变长变化过程，新增的对角数字
+根据题目，只考虑变长为奇数的情况，因为当边长为偶数时，两条斜对角不会交叉于某个数字
+0-->1   初始化  1
+1-->2   2的平方-1=3
+2-->3   2的平方+1=5    3的平方=9    3的平方-2=7
+3-->4   4的平方-3=13
+4-->5   4的平方+1=17    5的平方=25    5的平方-4=21
+5-->6   6的平方-5=31
+6-->7   6的平方+1=37    7的平方=49    7的平方-6=43
+7-->8   57
+'''
+
 starttime = datetime.datetime.now()
 
-x=123456
-n=1
-ctArr=[]
-while True:
-    for r in range(1,n+1):
-        numb=getNum(n,r)
-        if numb>1000000:
-            print str(n)+':'+str(r)+":"+str(numb)
-            ctArr.append(numb)
-    n+=1
-    if n>100:
-        break
+def isPrime(x):
+    for tmp in range(2,int(math.sqrt(x))+1):
+        if x%tmp==0:
+            return False
+    return True
+    
+primeCount=0  
+allCount=1
+base=set()
+base.add(1)
+n=2 
+per=100
+while per>=10:
+    if n%2==0:
+        num=n**2-(n-1)
+        base.add(num)
+        allCount+=1
+        if isPrime(num):
+            primeCount+=1
+            
+    else:
+        num1=(n-1)**2+1
+        num2=n**2-(n-1)
+        num3=n**2
+        base.add(num1)
+        base.add(num2)
+        base.add(num3)
+        allCount+=3 #n的平方不可能是质数
+        if isPrime(num1):
+            primeCount+=1
+            
+        if isPrime(num2):
+            primeCount+=1
+        
+    n+=1 
+    per=primeCount*100/allCount
+    #print per
+    
+    
+print n-1   
 
-print '-----------------'
-print len(ctArr)        
 print '-----------------'
 endtime = datetime.datetime.now()
-print('All spent '+str((endtime-starttime).microseconds)+' microseconds')
-
-
-
+print('All spent '+str((endtime-starttime).seconds)+' seconds')
